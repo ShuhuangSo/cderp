5088a95e3bfb467030f2e26ee8bdb368a0e83a2d
sushuhuang <shuhuang.so@qq.com>
2024-10-29 15:39:36 +0800

    v161

--- src/views/app/mercado/MelPostcodeArea.vue
+++ src/views/app/mercado/MelPostcodeArea.vue
@@ -1,28 +1,33 @@
 <template>
   <div>
-    <div style="margin: 10px">
-      <el-input size="medium" placeholder="邮编"
-                clearable
-                @clear="clearSearchValue"
-                @keyup.enter.native="doSearch"
-                v-model="postcode"
-                style="width: 300px; margin-right: 5px">
-        <el-button slot="append" icon="el-icon-search" @click="doSearch">查询</el-button>
-      </el-input>
-      <span style="font-size: 14px; color: #8a8a8a; margin-left: 15px">计算运费</span>
-      <el-switch
-          style="margin-left: 5px"
-          size="medium"
-          @change="doSearch"
-          v-model="postage"
-          >
-      </el-switch>
-      <span v-if="postage" style="font-size: 14px; color: #8a8a8a; margin-left: 15px">重量g</span>
-      <el-input-number v-model="weight"
-                       v-if="postage"
-                       style="margin-left: 5px"
-                       size="mini"
-                       controls-position="right" :min="1"></el-input-number>
+    <div style="margin: 10px;display: flex; justify-content: space-between">
+      <div>
+        <el-input size="medium" placeholder="邮编"
+                  clearable
+                  @clear="clearSearchValue"
+                  @keyup.enter.native="doSearch"
+                  v-model="postcode"
+                  style="width: 300px; margin-right: 5px">
+          <el-button slot="append" icon="el-icon-search" @click="doSearch">查询</el-button>
+        </el-input>
+        <span style="font-size: 14px; color: #8a8a8a; margin-left: 15px">计算运费</span>
+        <el-switch
+            style="margin-left: 5px"
+            size="medium"
+            @change="doSearch"
+            v-model="postage"
+        >
+        </el-switch>
+        <span v-if="postage" style="font-size: 14px; color: #8a8a8a; margin-left: 15px">重量g</span>
+        <el-input-number v-model="weight"
+                         v-if="postage"
+                         style="margin-left: 5px"
+                         size="mini"
+                         controls-position="right" :min="1"></el-input-number>
+      </div>
+      <div>
+        <el-button @click="uploadVisible=true">批量查询分区</el-button>
+      </div>
 
     </div>
     <div v-if="data_list.length">
@@ -99,6 +104,67 @@
 
       </el-table>
     </div>
+
+    <!--    批量上传弹窗-->
+    <el-dialog
+        title="批量上传"
+        :visible.sync="uploadVisible"
+        :destroy-on-close="true"
+        :close-on-click-modal="false"
+        width="500px"
+    >
+      <div>
+        <el-upload
+            v-loading="loading"
+            style="margin-left: 50px;"
+            ref="upload"
+            name="excel"
+            :headers="headers"
+            :data="{'carrier_code': this.carrier, 'is_remote': this.is_remote}"
+            :on-success="onSuccess"
+            :auto-upload="false"
+            :before-upload="beforeUpload"
+            accept=".xlsx"
+            :limit="1"
+            drag
+            action="api/platform_category/bulk_get_postcode_area/">
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+          <div class="el-upload__tip" slot="tip">只能上传excel文件,
+            <el-link href="/media/template/bulk_get_postcode_upload_template.xlsx" target="_blank">模板下载</el-link>
+            <div>
+              请选择要查询分区的渠道：
+              <el-select v-model="carrier"
+                         style="width: 100px;margin-left: 5px"
+                         placeholder="请选择筛选项">
+                <el-option
+                    v-for="item in carrier_group"
+                    :key="item.name"
+                    :label="item.name"
+                    :value="item.value">
+                </el-option>
+              </el-select>
+            </div>
+            <div>
+              仅筛选3,4偏远分区
+              <el-switch
+                  v-model="is_remote"
+                  active-color="#13ce66"
+                  inactive-color="#ff4949">
+              </el-switch>
+            </div>
+          </div>
+
+
+        </el-upload>
+
+      </div>
+      <span slot="footer" class="dialog-footer">
+          <el-button size="small" @click="uploadVisible = false">取 消</el-button>
+          <el-button size="small" type="primary" :disabled="loading" @click="submitUpload">开始导入</el-button>
+        </span>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -114,6 +180,23 @@ export default {
       loading: false,
       postage: false,
       weight: 100,
+      uploadVisible: false,
+      // 批量上传认证
+      headers: {
+        Authorization: window.localStorage.getItem('tokenStr'),
+      },
+      carrier: 'UBI_AUS_POST',
+      is_remote: true, //仅查看偏远分区
+      carrier_group: [
+        {
+          name: '澳邮',
+          value: 'UBI_AUS_POST'
+        },
+        {
+          name: 'Aramex',
+          value: 'UBI_ARAMEX'
+        }
+      ],
     }
   },
   filters:{
@@ -132,6 +215,28 @@ export default {
     },
   },
   methods:{
+    // 型号上传前的回调
+    beforeUpload() {
+      this.loading = true;
+    },
+    // 上传成功之后的回调
+    onSuccess(response, file, fileList) {
+      this.loading = false;
+      this.$refs.upload.clearFiles();
+      if (response.status === 'error') {
+        this.$message.error(response.msg)
+      }
+      this.uploadVisible = false;
+      if (response.status === 'success') {
+        window.open(response.url, '_blank')
+        this.$message.success(response.msg)
+      }
+
+    },
+    // 上传
+    submitUpload() {
+      this.$refs.upload.submit();
+    },
     clearSearchValue(){
       this.postcode = ''
       this.data_list = []
